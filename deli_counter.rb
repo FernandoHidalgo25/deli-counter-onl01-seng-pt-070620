a008930784b3ab0f4958c5fc8ac704cedbeca8e4
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-14 04:24:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,19 @@
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  counter = 1
-    katz_deli.each_with_index do |name|
-    puts "Welcome, #{name}. You are number #{counter} in line."
-    counter += 1
-  end
-end
-def now_serving(katz_deli)
-  if katz_deli.size == 0
-    puts "There is nobody waiting to be served!"
-  else
-  puts "Currently serving #{katz_deli[0]}."
-  katz_deli.shift[0]
-end
+
+
+
+#def take_a_number(katz_deli, name)
+  #katz_deli.push(name)
+  #counter = 1
+    #katz_deli.each_with_index do |name|
+    #puts "Welcome, #{name}. You are number #{counter} in line."
+    #counter += 1
+  #end
+#end
+#def now_serving(katz_deli)
+  #if katz_deli.size == 0
+    #puts "There is nobody waiting to be served!"
+  #else
+  #puts "Currently serving #{katz_deli[0]}."
+ #katz_deli.shift[0]
+#end
   
