d268dd3a1424c6699b2937f6328d331cfe41e1ea
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-14 04:02:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,16 @@
-def line(katz_deli)
+def line(katz_deli, name)
+  katz_deli.push(name)
+  counter = 1
+    katz_deli.each_with_index do |name|
+    puts "Welcome, #{name}. You are number #{counter} in line."
+    counter += 1
+  end
+end
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{katz_deli[0]}."
+  katz_deli.unshift[0]
+end
   
